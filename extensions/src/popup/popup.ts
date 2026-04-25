42cf9f2a73c4d7d307f030bf87f0292aab64a7f1
Eddie Taliaferro <eddielacrosse2@gmail.com>
2026-04-25 18:06:57 -0400

    Created popup and lib folders with components and html scafford

---
+++ extensions/src/popup/popup.ts
@@ -0,0 +1,7 @@
+import App from '$lib/Popup.svelte';
+
+const app = new App({
+	target: document.getElementById('app')!
+});
+
+export default app;
--- extensions/vite.config.ts
+++ extensions/vite.config.ts
@@ -7,6 +7,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      $lib: resolve(__dirname, 'src/lib')
+    }
+  },
   build: {
     rollupOptions: {
       input: {
