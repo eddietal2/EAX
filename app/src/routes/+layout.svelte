34fb8cd0876749514075cf35c768a367cd662124
Eddie Taliaferro <eddielacrosse2@gmail.com>
2026-04-30 11:36:43 -0400

    Updated Favicon

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 	import Navigation from '$lib/components/Navigation.svelte';
 	import Toast from '$lib/components/Toast.svelte';
 
